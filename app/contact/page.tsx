e1556a85d4bf4bf087b440a8b27ddd0a51a6f30f
Mikaruz <gersonsebas2019@gmail.com>
2024-12-25 14:54:43 -0500

    Add SEO metadata to Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SEO Title",
+  description: "SEO Title",
+  keywords: ["SEO", "Title"],
+};
+
 export default function ContactPage() {
   return (
     <div>
